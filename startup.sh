003ec305e6e23369bcc4ae99a0d18ee150beb625
interviewparrot <amrita@interviewparrot.com>
2019-10-19 06:35:08 -0400

    Adding docker compose to start zookeeper and kafka

--- startup.sh
+++ startup.sh
@@ -2,7 +2,9 @@
 echo $PWD
 KAFKA_HOME=/opt/kafka
 echo $KAFKA_HOME
+echo $ZOOKEEPER_CONNECT
 cd $KAFKA_HOME
-ls
+sed -i "s/^zookeeper.connect=.*/zookeeper.connect=k8scale-zookeeper-1:2181/g" $KAFKA_HOME/config/server.properties
+cat $KAFKA_HOME/config/server.properties | grep 'zookeeper.connect'
 sh $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
 
